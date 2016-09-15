617326faaed5703ef57f0ff97412082b3ce35737
Ben Irving <jammin.irving@gmail.com>
2016-09-15 09:44:20 -0700

    Remove tls.conf file

--- jessie/entrypoint.py
+++ jessie/entrypoint.py
@@ -14,6 +14,7 @@ certbot_cmd = [
     "certonly",
     "--standalone",
     "--agree-tos",
+    "--must-staple",
     "--rsa-key-size", "4096",
     "--post-hook", "post-hook.sh",
 ]
@@ -26,24 +27,56 @@ nginx_cmd = ["nginx", "-g", "daemon off;"]
 # template
 server_conf = """
 server {{
+    # Based on https://mozilla.github.io/server-side-tls/ssl-config-generator/
+    # for Nginx 1.11.3
+    
     listen 443 ssl http2;
     listen [::]:443 ssl http2;
     server_name {0};
+
+    ssl_session_timeout 1d;
+    ssl_session_cache shared:SSL:50m;
+    ssl_session_tickets off;
     ssl_certificate {1};
     ssl_certificate_key {2};
+    ssl_trusted_certificate {3};
+
+    # intermediate configuration. tweak to your needs.
+    ssl_protocols TLSv1 TLSv1.1 TLSv1.2;
+    ssl_ciphers 'ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:DHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-AES128-SHA256:ECDHE-RSA-AES128-SHA256:ECDHE-ECDSA-AES128-SHA:ECDHE-RSA-AES256-SHA384:ECDHE-RSA-AES128-SHA:ECDHE-ECDSA-AES256-SHA384:ECDHE-ECDSA-AES256-SHA:ECDHE-RSA-AES256-SHA:DHE-RSA-AES128-SHA256:DHE-RSA-AES128-SHA:DHE-RSA-AES256-SHA256:DHE-RSA-AES256-SHA:ECDHE-ECDSA-DES-CBC3-SHA:ECDHE-RSA-DES-CBC3-SHA:EDH-RSA-DES-CBC3-SHA:AES128-GCM-SHA256:AES256-GCM-SHA384:AES128-SHA256:AES256-SHA256:AES128-SHA:AES256-SHA:DES-CBC3-SHA:!DSS';
+    ssl_prefer_server_ciphers on;
+
+    # fetch OCSP records from URL in ssl_certificate and cache them
+    ssl_stapling on;
+    ssl_stapling_verify on;
+
+    # Diffie-Hellman parameter for DHE ciphersuites, recommended 2048 bits
+    ssl_dhparam /etc/ssl/certs/dhparam.pem;
+
+    # HSTS (ngx_http_headers_module is required) (15768000 seconds = 6 months)
+    add_header Strict-Transport-Security max-age=15768000;
 
     # If there is a conf file, these directives will
     # be added into the server block. If there is
     # no include directive here then you need to 
     # create a conf file mounted in a volume in
     # /etc/nginx/conf.d/custom/
-    {3}
+    {4}
 }}
+"""
+
+redirect_conf = """
+server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    return 301 https://$host$request_uri;
+}
 """
 
 def create_conf(domain):
     fullchain = os.path.join(LIVE_DIR, domain, 'fullchain.pem')
     privkey = os.path.join(LIVE_DIR, domain, 'privkey.pem')
+    chain = os.path.join(LIVE_DIR, domain, 'chain.pem')
 
     custom_include = ""
     if os.path.exists("/etc/nginx/conf.d/custom/"):
@@ -54,9 +87,13 @@ def create_conf(domain):
         fd.write(server_conf.format(
             domain, 
             fullchain, 
-            privkey, 
+            privkey,
+            chain, 
             custom_include))
 
+    # create a redirect for http traffic
+    with open("/etc/nginx/conf.d/redirect.conf", "w") as fd:
+        fd.write(redirect_conf)
 
 def fail_with_error_message(msg):
     logging.error(msg)
