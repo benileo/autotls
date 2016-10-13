fdb2af4cc89dee22460bcbe4f4318482f7868430
Ben Irving <jammin.irving@gmail.com>
2016-10-13 13:38:03 -0700

    Revert "Remove redirect again!" (#14)

--- entrypoint.py
+++ entrypoint.py
@@ -54,6 +54,14 @@ server {{
 }}
 """
 
+redirect_conf = """
+server {{
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    return 301 https://{0}$request_uri;
+}}
+"""
+
 
 class Certbot(object):
     @classmethod
@@ -138,6 +146,8 @@ class Certbot(object):
 
 
 def create_conf(certbot):
+    create_redirect(certbot.domain)
+
     custom_include = ""
     if os.path.exists("/etc/nginx/conf.d/custom/"):
         logging.info("Including custom configuration")
@@ -159,6 +169,12 @@ def create_conf(certbot):
 def fail_with_error_message(msg):
     logging.error(msg)
     sys.exit(1)
+
+
+def create_redirect(domain):
+    with open("/etc/nginx/conf.d/redirect.conf", "w") as fd:
+        fd.write(redirect_conf.format(domain))
+    logging.info("http redirect created")
 
 
 processes = []
